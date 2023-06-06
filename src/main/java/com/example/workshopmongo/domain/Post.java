0dc06c16f4b1a6b22d0054ad9d20cc951718a6a3
Gabriel Gomes <gabriel.grohl2006@gmail.com>
2023-06-06 18:26:47 -0300

    adding comments to the posts

--- src/main/java/com/example/workshopmongo/domain/Post.java
+++ src/main/java/com/example/workshopmongo/domain/Post.java
@@ -1,14 +1,17 @@
 package com.example.workshopmongo.domain;
 
 import com.example.workshopmongo.dto.AuthorDTO;
+import com.example.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
-@Document(collection = "posts")
+@Document(collection = "post")
 public class Post implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -20,6 +23,7 @@ public class Post implements Serializable {
     private String body;
     private AuthorDTO author;
 
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
 
@@ -71,6 +75,14 @@ public class Post implements Serializable {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-@Document(collection ="users")
+@Document(collection ="user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
